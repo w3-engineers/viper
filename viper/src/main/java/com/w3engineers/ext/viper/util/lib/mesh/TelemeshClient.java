70bcb31ef16f16de7f7bba22626f806f51b3fa54
W3-Monir <moniruzzaman@w3engineers.com>
2019-10-18 15:25:40 +0600

    -- api creation for Telemesh client

--- viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/TelemeshClient.java
+++ viper/src/main/java/com/w3engineers/ext/viper/util/lib/mesh/TelemeshClient.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import com.w3engineers.ext.viper.application.data.remote.model.MeshPeer;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class TelemeshClient {
 
     private static TelemeshClient mTelemeshClient;
@@ -30,5 +33,50 @@ public class TelemeshClient {
 
     // Todo: here we will set all the api to contact with application and at the same time we will take the decision which AIDL interface have to call
 
+    public void sendMessage(String senderId, String receiverId, String messageId, byte[] data) {
+
+    }
+
+
+    public int getLinkTypeById(String nodeID) {
+
+        return 0;
+    }
+
+    /**
+     * Listener is initialise here to get all kind of callback here
+     * and after that send callback to app level
+     */
+
+    LinkStateListener linkStateListener = new LinkStateListener() {
+
+        @Override
+        public void onLocalUserConnected(String nodeId, byte[] data) {
+
+
+        }
+
+        @Override
+        public void onRemoteUserConnected(String nodeId) {
+
+        }
+
+        @Override
+        public void onUserDisconnected(String nodeId) {
+
+        }
+
+        @Override
+        public void onMessageReceived(String senderId, byte[] frameData) {
+
+        }
+
+
+        @Override
+        public void onMessageDelivered(String messageId, int status) {
+
+        }
+
+    };
 
 }
