7770c7751f774ba8cc5f33e18e4c87aacf839492
W3-MajorArif <arifujjaman@w3engineers.com>
2019-10-22 12:18:43 +0600

    feat: (Need merge from other branch)
    We need to take merge from api_expose branch to current branch.
    
    It is necessary for system upgradation.

--- viper/src/main/java/com/w3engineers/mesh/application/data/local/purchase/PurchaseManager.java
+++ viper/src/main/java/com/w3engineers/mesh/application/data/local/purchase/PurchaseManager.java
@@ -1,4 +1,16 @@
 package com.w3engineers.mesh.application.data.local.purchase;
 
+import android.content.Context;
+
+import com.w3engineers.eth.data.remote.EthereumService;
+import com.w3engineers.mesh.application.data.local.db.DatabaseService;
+import com.w3engineers.mesh.application.data.local.helper.PreferencesHelper;
+
 public class PurchaseManager {
+
+    private PayController payController;
+    private EthereumService ethService;
+    private Context mContext;
+    private DatabaseService databaseService;
+    private PreferencesHelper preferencesHelper;
 }
