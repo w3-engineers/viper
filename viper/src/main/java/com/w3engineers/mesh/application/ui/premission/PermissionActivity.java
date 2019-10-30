719b3251118d73e416c592f48962660889dffbd8
W3-Monir <moniruzzaman@w3engineers.com>
2019-10-30 17:51:38 +0600

    feat(dynamic app name): provide app name from application layer
    * app name would be provide from application layer
    * app name pass through Telemesh service client

--- viper/src/main/java/com/w3engineers/mesh/application/ui/premission/PermissionActivity.java
+++ viper/src/main/java/com/w3engineers/mesh/application/ui/premission/PermissionActivity.java
@@ -2,48 +2,24 @@ package com.w3engineers.mesh.application.ui.premission;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import com.w3engineers.ext.strom.util.helper.PermissionUtil;
-import com.w3engineers.mesh.R;
 import com.w3engineers.mesh.util.lib.mesh.ViperClient;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import static com.w3engineers.mesh.util.lib.mesh.ViperClient.appName;
+import static com.w3engineers.mesh.util.lib.mesh.ViperClient.networkPrefix;
 
 public class PermissionActivity extends AppCompatActivity {
-    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
-    public static final int REQUEST_ID_WRITE_SETTINGS = 106;
-    public static final int REQUEST_ID_LOCATION_PERMISSION = 201;
-    public static final int REQUEST_OVERLAY_PERMISSSION = 202;
-    private static final int REQUEST_ENABLE_DSC = 107;
-    private AlertDialog mAlertDialog;
-    private BluetoothAdapter mBluetoothAdapter = null;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         checkPermission();
     }
-
 
     private void checkPermission() {
         if (PermissionUtil.on(this).request(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
@@ -52,7 +28,7 @@ public class PermissionActivity extends AppCompatActivity {
     }
 
     private void closeCurrentActivity() {
-        ViperClient.on(this, "").startClient();
+        ViperClient.on(this, appName, networkPrefix).startClient();
         finish();
     }
 
@@ -60,5 +36,4 @@ public class PermissionActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         checkPermission();
     }
-
 }
--- viper/src/main/java/com/w3engineers/mesh/util/lib/mesh/ViperClient.java
+++ viper/src/main/java/com/w3engineers/mesh/util/lib/mesh/ViperClient.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.RemoteException;
-import android.widget.Toast;
 
 import com.w3engineers.ext.strom.util.helper.PermissionUtil;
 import com.w3engineers.mesh.application.data.local.DataPlanConstants;
@@ -12,14 +11,13 @@ import com.w3engineers.mesh.application.data.local.helper.PreferencesHelperDatap
 import com.w3engineers.mesh.application.data.local.purchase.PurchaseManagerBuyer;
 import com.w3engineers.mesh.application.data.local.purchase.PurchaseManagerSeller;
 import com.w3engineers.mesh.application.data.local.wallet.WalletManager;
-import com.w3engineers.mesh.application.data.local.wallet.WalletService;
 import com.w3engineers.mesh.application.ui.premission.PermissionActivity;
-import com.w3engineers.mesh.util.MeshApp;
 
 public class ViperClient {
 
     private Context mContext;
-    private String networkPrefix;
+    public static String networkPrefix;
+    public static String appName;
     private static ViperClient mViperClient;
 
     private ViperClient() {
@@ -29,21 +27,32 @@ public class ViperClient {
         }
     }
 
-    protected ViperClient(Context context, String networkPrefix) {
+    protected ViperClient(Context context, String appName, String networkPrefix) {
         this.mContext = context;
+        this.appName = appName;
         this.networkPrefix = networkPrefix;
 
-        if (PermissionUtil.on(context).isAllowed(Manifest.permission.READ_EXTERNAL_STORAGE)){
+        if (PermissionUtil.on(context).isAllowed(Manifest.permission.READ_EXTERNAL_STORAGE)) {
             startClient();
-        }else {
+        } else {
             startPermissionActivity(context);
         }
     }
 
+    public static ViperClient on(Context context, String appName, String networkPrefix) {
+        if (mViperClient == null) {
+            synchronized (ViperClient.class) {
+                if (mViperClient == null)
+                    mViperClient = new ViperClient(context, appName, networkPrefix);
+            }
+        }
+        return mViperClient;
+    }
 
-    public void startClient(){
 
-        DataManager.on().doBindService(mContext, this.networkPrefix);
+    public void startClient() {
+
+        DataManager.on().doBindService(mContext, appName, this.networkPrefix);
         WalletManager.getInstance().readWallet(mContext);
         if (PreferencesHelperDataplan.on().getDataPlanRole() == DataPlanConstants.USER_ROLE.DATA_SELLER) {
             PurchaseManagerSeller.getInstance().setPayControllerListener();
@@ -54,22 +63,13 @@ public class ViperClient {
         }
     }
 
-    private void startPermissionActivity(Context context){
+    private void startPermissionActivity(Context context) {
         Intent permissionIntent = new Intent(context, PermissionActivity.class);
         permissionIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(permissionIntent);
 
     }
 
-
-    public static ViperClient on(Context context, String networkPrefix) {
-        if (mViperClient == null) {
-            synchronized (ViperClient.class) {
-                if (mViperClient == null) mViperClient = new ViperClient(context, networkPrefix);
-            }
-        }
-        return mViperClient;
-    }
 
     public void sendMessage(String senderId, String receiverId, String messageId, byte[] data) throws RemoteException {
         DataManager.on().sendData(senderId, receiverId, messageId, data);
@@ -83,7 +83,5 @@ public class ViperClient {
     public String getUserId() throws RemoteException {
         return DataManager.on().getUserId();
     }
-
-
 
 }
