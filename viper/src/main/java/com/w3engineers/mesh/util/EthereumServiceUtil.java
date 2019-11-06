8bd5f9dfd2e1a4b919b80e8cc7ec85bcd28f8e37
W3 Mimo <mimosaha@w3engineers.com>
2019-11-06 13:10:12 +0600

    fix(Modify airdrop process): Update airdrop process in pay lib
     - Update airdrop calling process
     - Switch build config from pay lib to viper
    
    Relates #26

--- viper/src/main/java/com/w3engineers/mesh/util/EthereumServiceUtil.java
+++ viper/src/main/java/com/w3engineers/mesh/util/EthereumServiceUtil.java
@@ -12,6 +12,7 @@ import android.content.Context;
 
 import com.w3engineers.eth.data.helper.model.PayLibNetworkInfo;
 import com.w3engineers.eth.data.remote.EthereumService;
+import com.w3engineers.mesh.BuildConfig;
 import com.w3engineers.mesh.application.data.local.db.DatabaseService;
 import com.w3engineers.mesh.application.data.local.db.networkinfo.NetworkInfo;
 
@@ -38,7 +39,7 @@ public class EthereumServiceUtil implements EthereumService.NetworkInfoCallback 
     private EthereumServiceUtil(Context context) {
         databaseService = DatabaseService.getInstance(context);
         populateDb(context);
-        ethereumService = EthereumService.getInstance(context, this);
+        ethereumService = EthereumService.getInstance(context, this, BuildConfig.GIFT_DONATE_LINK);
     }
 
     public static EthereumServiceUtil getInstance(Context context) {
