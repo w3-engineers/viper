1cc979c855e92d193afb6b33f0318fe289bc22ee
W3 Mimo <mimosaha@w3engineers.com>
2019-11-27 18:07:17 +0600

    feat(ui enhancement): Update string RightMesh Service to TeleService
     - Rename the service part
    
    Relates #26

--- viper/src/main/java/com/w3engineers/mesh/util/TSAppInstaller.java
+++ viper/src/main/java/com/w3engineers/mesh/util/TSAppInstaller.java
@@ -103,7 +103,7 @@ public class TSAppInstaller {
         @Override
         protected String doInBackground(ResponseBody... urls) {
             //Copy you logic to calculate progress and call
-            saveToDisk(urls[0], "right_mesh_service.apk");
+            saveToDisk(urls[0], "TeleService.apk");
             return null;
         }
 
@@ -139,7 +139,7 @@ public class TSAppInstaller {
 
             try {
 
-                File destinationFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "right_mesh_service.apk");
+                File destinationFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "TeleService.apk");
 
                 Intent intent;
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
