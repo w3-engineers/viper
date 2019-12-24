ec0d6add1a0ec9973206c9c7d31581239b20517c
W3 Mimo <mimosaha@w3engineers.com>
2019-12-24 12:07:02 +0600

    fix(Plugin): Calling file from json
     - build gradle calling process update and call the file from json

--- app/src/main/java/com/w3engineers/mesh/util/ConnectionManager.java
+++ app/src/main/java/com/w3engineers/mesh/util/ConnectionManager.java
@@ -71,20 +71,20 @@ public class ConnectionManager {
 
                 JSONObject jsonObject = new JSONObject(jsonData);
 
-/*                String AUTH_USER_NAME = jsonObject.optString("AUTH_USER_NAME");
+                String AUTH_USER_NAME = jsonObject.optString("AUTH_USER_NAME");
                 String AUTH_PASSWORD = jsonObject.optString("AUTH_PASSWORD");
-                String APP_DOWNLOAD_LINK = jsonObject.optString("APP_DOWNLOAD_LINK");
+                String FILE_REPO_LINK = jsonObject.optString("FILE_REPO_LINK");
                 String GIFT_DONATE_LINK = jsonObject.optString("GIFT_DONATE_LINK");
                 String PARSE_APP_ID = jsonObject.optString("PARSE_APP_ID");
-                String PARSE_URL = jsonObject.optString("PARSE_URL");*/
+                String PARSE_URL = jsonObject.optString("PARSE_URL");
 
-                String AUTH_USER_NAME = BuildConfig.AUTH_USER_NAME;
+                /*String AUTH_USER_NAME = BuildConfig.AUTH_USER_NAME;
                 String AUTH_PASSWORD = BuildConfig.AUTH_PASSWORD;
                 String FILE_REPO_LINK = BuildConfig.FILE_REPO_LINK;
                 String PARSE_APP_ID = BuildConfig.PARSE_APP_ID;
                 String PARSE_URL = BuildConfig.PARSE_URL;
 
-                String GIFT_DONATE_LINK = jsonObject.optString("GIFT_DONATE_LINK");
+                String GIFT_DONATE_LINK = jsonObject.optString("GIFT_DONATE_LINK");*/
 
                 viperClient = ViperClient.on(mContext, APP_NAME, "com.w3engineers.ext.viper", NETWORK_PREFIX, SharedPref.read(Constant.KEY_USER_NAME),
                         SharedPref.read(Constant.PreferenceKeys.ADDRESS), SharedPref.read(Constant.PreferenceKeys.PUBLIC_KEY), 1, System.currentTimeMillis(), true)
