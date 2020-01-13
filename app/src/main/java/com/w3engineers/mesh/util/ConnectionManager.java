eae969d4a469572ec0d5a51c771a172cdd72b89b
moniruzzaman@w3engineers.com <&s85jmoUD9r8>
2020-01-13 11:25:29 +0600

    feat(.so file for credentials) all credentials now added into a cpp file
    -- All credentials are now inside a .so file

--- app/src/main/java/com/w3engineers/mesh/util/ConnectionManager.java
+++ app/src/main/java/com/w3engineers/mesh/util/ConnectionManager.java
@@ -29,8 +29,6 @@ import com.w3engineers.mesh.util.lib.mesh.ViperClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
