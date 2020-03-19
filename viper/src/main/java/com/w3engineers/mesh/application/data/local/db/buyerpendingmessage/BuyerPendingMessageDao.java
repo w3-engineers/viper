130d8fa3b2a5221fae29bf0cab855168bf5b4053
W3-MajorArif <arifujjaman@w3engineers.com>
2020-03-19 10:57:46 +0600

    buyer pending message issue solved.

--- viper/src/main/java/com/w3engineers/mesh/application/data/local/db/buyerpendingmessage/BuyerPendingMessageDao.java
+++ viper/src/main/java/com/w3engineers/mesh/application/data/local/db/buyerpendingmessage/BuyerPendingMessageDao.java
@@ -13,10 +13,10 @@ public interface BuyerPendingMessageDao {
     @Query("SELECT * FROM buyerpendingmessage WHERE msg_id =:msgID")
     public BuyerPendingMessage getMsgById(String msgID);
 
-    @Query("SELECT * FROM buyerpendingmessage WHERE status =:status ORDER BY id ASC LIMIT 1")
+    @Query("SELECT * FROM buyerpendingmessage WHERE status =:status ORDER BY create_time ASC LIMIT 1")
     public BuyerPendingMessage getMsgByStatus(int status);
 
-    @Query("SELECT * FROM buyerpendingmessage WHERE status =:status AND ( (is_incomming=1 AND owner =:userAddress) OR ( is_incomming=0 AND sender=:userAddress ))  ORDER BY id ASC LIMIT 1")
+    @Query("SELECT * FROM buyerpendingmessage WHERE status =:status AND ( (is_incomming=1 AND owner =:userAddress) OR ( is_incomming=0 AND sender=:userAddress ))  ORDER BY create_time ASC LIMIT 1")
     public BuyerPendingMessage getBuyerPendingMessageByUser(int status, String userAddress);
 
     @Update
